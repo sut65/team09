625c7f3d2b8aa324a2756f49220b50bab515b8da
unknown <anuchakleawthong@gmail.com>
2023-01-22 12:01:15 +0700

    update - colse #22

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import Navbar from "./components/PatientSchedule";
+
+
+
+export default function App() {
+
+return (
+
+  <Router>
+
+   <div>
+
+   <Navbar />
+
+   <Routes>
+
+    
+
+   </Routes>
+
+   </div>
+
+  </Router>
+
+);
+
 }
-
-export default App;
