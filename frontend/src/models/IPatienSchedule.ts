075077ef09fe82c885472ce17c3acc9c728b0b27
unknown <anuchakleawthong@gmail.com>
2023-01-27 15:22:24 +0700

    update - close #121

--- frontend/src/models/IPatienSchedule.ts
+++ frontend/src/models/IPatienSchedule.ts
@@ -1,9 +1,23 @@
 import { ReasonInterface } from "./IReason";
+import { EmployeeInterface } from "./IEmployee"
+import { Type_of_treatments_Interface } from "./IType_of_treatment"
+import { PatientInterface } from "./IPatient"
 
 export interface PatienSceheduleInterface {
     ID?: number;
    
+    PatientID?: number;
+    Patient?:   PatientInterface;
+
+    EmployeeID?: number;
+	  Employee?:   EmployeeInterface ;
+
     ReasonID?: number;
     Reason?: ReasonInterface;
+
+    Type_Of_TreatmentID?: number,
+   	Type_Of_Treatment?:   Type_of_treatments_Interface;
+
+    Date_time: Date | null;
     
   }
