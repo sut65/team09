e017ff9075a2ba09f836b50ce4598a5349fbb8ed
B6309985 <B6309985@g.sut.ac.th>
2023-01-28 09:18:15 +0700

    เเก้ไขหน้า UI Treatment.tsx เเละ controller treatment.go - close #27

--- frontend/src/components/treatment/Treatment.tsx
+++ frontend/src/components/treatment/Treatment.tsx
@@ -1,6 +1,4 @@
 import React, { useEffect } from "react";
-//import { useHistory } from "react-router-dom";
-
 import { Link, Link as RouterLink } from "react-router-dom";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
@@ -11,7 +9,7 @@ import { DataGrid, GridColDef } from "@mui/x-data-grid";
 import axios from 'axios';
 import { ButtonGroup } from "@mui/material";
 
-function Treatment(props: any) {
+function Treatment(props: any){
     const [treatment, setTreatment] = React.useState<TreatmentsInterface[]>([]);
     const getTreatment = async () => {
         const apiUrl = "http://localhost:8080/treatments";
@@ -58,53 +56,40 @@ function Treatment(props: any) {
     const columns: GridColDef[] = [
         { field: "id", headerName: "ID", width: 70 },
 
-        { field: "dentist_name", headerName: "Dentist", width: 70 },
+        { field: "dentist_name", headerName: "Dentist", width: 100 },
 
-        { field: "patient_name", headerName: "Patient", width: 70 },
+        { field: "patient_name", headerName: "Patient", width: 100 },
 
         { field: "number_of_cavities", headerName: "Number of cavities", width: 130 },
 
-        { field: "number_of_swollen_gums", headerName: "Number of swollen_gums", width: 150 },
+        { field: "number_of_swollen_gums", headerName: "Number of swollen_gums", width: 180 },
 
-        { field: "number_of_treatment", headerName: "number_of_treatment", width: 200 },
+        { field: "other_teeth_problems", headerName: "Other teeth problems", width: 150 },
 
-        { field: "other_teeth_problems", headerName: "Other teeth problems", width: 130 },
+        { field: "type_of_treatment_name", headerName: "Type of treatment", width: 160 },
 
-        { field: "type_of_treatment_name", headerName: "Type of treatment", width: 130 },
+        { field: "number_of_treatment", headerName: "Number of treatment", width: 150 },
 
-
-
-
-
-        { field: "treatment_detail", headerName: "Treatment", width: 90 },
-
-        { field: "treatment_time", headerName: "Treatment Time", width: 120 },
+        { field: "type_of_number_of_treatment_name", headerName: "Type of number of treatment", width: 200 },
+        
+        { field: "treatment_detail", headerName: "Treatment detail", width: 150 },
+ 
+        { field: "treatment_time", headerName: "Treatment Time", width: 200 },
 
         { field: "treatment_code", headerName: "Treatment code", width: 120 },
 
-        // {
-        //     field: "action", headerName: "Action",width: 200, sortable: false, renderCell: ({ row }) =>
-        //     <ButtonGroup>
-        //         <Button onClick={() => handleDelete(row.id)}>
-        //             delete
-        //         </Button>
-        //         <Button >
-        //             update
-        //         </Button>
-        //     </ButtonGroup>
-        // },
         {
-            field: "action", headerName: "Action", width: 200, sortable: false, renderCell: ({ row }) =>
-                <ButtonGroup>
-                    <Button onClick={() => handleDelete(row.id)}>
-                        delete
-                    </Button>
-                    <Button component={RouterLink} to={`/treatmentupdate/${row.id}`} variant="contained">
-                        <div className="good-font">
-                            update
-                        </div>
-                    </Button>
-                </ButtonGroup>
+            field: "action", headerName: "Action",width: 200, sortable: false, renderCell: ({ row }) =>
+            <ButtonGroup>
+                <Button onClick={() => handleDelete(row.id)}>
+                    delete
+                </Button>
+                <Button component={RouterLink} to={`/treatmentsupdate/${row.id}`} variant="contained">
+                            <div className="good-font">
+                                update
+                            </div>
+                        </Button>
+            </ButtonGroup>
         },
 
     ];
@@ -142,7 +127,7 @@ function Treatment(props: any) {
                             color="primary"
                         >
                             <div className="good-font-white">
-                                เพิ่มข้อมูลการรักษา
+                                เพิ่มข้อการรักษา
                             </div>
                         </Button>
                     </Box>
